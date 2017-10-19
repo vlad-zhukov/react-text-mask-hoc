559640e9281e35712ab488999215ffee9e718f45
Vlad Zhukov <work4vlad@gmail.com>
2017-10-19 16:36:44 +0300

    Remove React Native from deps

--- __tests__/import-bundle-esm.js
+++ __tests__/import-bundle-esm.js
@@ -1,5 +1,4 @@
 import * as reactBundle from '../dist/react-text-mask-hoc.esm';
-import * as reactNativeBundle from '../ReactNative';
 
 describe('import-bundle-esm', () => {
     test('React bundle should export functions', () => {
@@ -8,13 +7,5 @@ describe('import-bundle-esm', () => {
         expect(typeof reactBundle.InputAdapter).toBe('function');
         expect(typeof reactBundle.SpanAdapter).toBe('function');
         expect(typeof reactBundle.TextMaskTransformer).toBe('function');
-    });
-
-    test('React Native bundle should export functions', () => {
-        expect(typeof reactNativeBundle.TextMask).toBe('function');
-        expect(typeof reactNativeBundle.withTextMask).toBe('function');
-        expect(typeof reactNativeBundle.TextInputAdapter).toBe('function');
-        expect(typeof reactNativeBundle.TextAdapter).toBe('function');
-        expect(typeof reactNativeBundle.TextMaskTransformer).toBe('function');
     });
 });
